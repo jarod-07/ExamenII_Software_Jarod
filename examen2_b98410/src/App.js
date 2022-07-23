692ec57e0f9f0fa817a8bba17d8cc286cf993a11
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-07-23 14:53:24 -0600

    Base del la app de react e implementacion base de la visualizacion pagina

---
+++ examen2_b98410/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { VendingMachine } from './Components/VendingMachine/VendingMachine';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <VendingMachine/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
